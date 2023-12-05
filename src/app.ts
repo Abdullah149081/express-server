7f3ee4a9760eb80c3df93fb0db1e192ce2afb738
Abdullah149081 <masudmd425@gmail.com>
2023-12-05 17:09:59 +0600

    routes refector

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import cors from "cors";
 import express, { Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
-import { studentRoutes } from "./app/modules/student/student.route";
-import { userRoutes } from "./app/modules/user/user.route";
+import router from "./app/routes";
 const app = express();
 
 //parsers
@@ -11,8 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use("/api/v1/student", studentRoutes);
-app.use("/api/v1/users", userRoutes);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
