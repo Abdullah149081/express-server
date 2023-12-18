7997eee1b4a388b4eab6aa0fa8bcd1c96a331bba
Abdullah149081 <masudmd425@gmail.com>
2023-12-18 19:31:36 +0600

    academic semester service post is done

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,13 +1,18 @@
 import httpStatus from "http-status";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
+import { academicSemesterService } from "./academicSemester.service";
 
 const createAcademicSemester = catchAsync(async (req, res) => {
+    const result = await academicSemesterService.createAcademicSemester(
+        req.body,
+    );
+
     sendResponse(res, {
         statusCode: httpStatus.OK,
         success: true,
         message: "Academic semester is created successfully",
-        data: "  message: 'Academic semester is created successfully',",
+        data: result,
     });
 });
 
